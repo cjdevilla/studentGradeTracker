7b02edc2eca487cb75eafad0b8abd18cab65569e
Camille Joy De Villa <devillacamillejoym@gmail.com>
2023-11-20 21:14:13 +0800

    added subject averages

--- grade_tracker.py
+++ grade_tracker.py
@@ -12,13 +12,13 @@ options = [
     "Exit"
 ]
 
-
-    
-
 def main():
-    students = array('u', [])
-    subjects = array("Math", "English" , "Science")
-    grades = array('u', [])
+    students = []
+    subjects = ["Math", "English" , "Science"]
+    grades = []
+
+    student_info = (student_name, subjects, array('f', [grades]))
+
     while True:
         for index, option in enumerate(options, start=1):
             print(f"{index}. {option}")
@@ -28,18 +28,17 @@ def main():
         if selected_option == 1:
             student_name = input("Enter student name: ")
             new_grades = [(subject, int(input(f"Enter {subject} grade: "))) for subject in subjects]
+            # grades.extend(new_grades)
             add_student(students, grades, student_name, new_grades)
         elif selected_option == 2:
             view_student_grades(students, grades)
         elif selected_option == 3:
-            view_subject_averages()
+            view_subject_averages(students ,grades, new_grades)
         elif selected_option == 4:
             identify_top_performers()
         elif selected_option == 6:
             print("Goodbye!")
             break
-
-
 
 def add_student(students, grades, student_name, new_grades):
     # student_name = input("Enter student name: ")
@@ -49,8 +48,7 @@ def add_student(students, grades, student_name, new_grades):
 
     # student_info = (student_name, array('f', [math_grade, english_grade, science_grade]))
     students.append(student_name) # "Joy" , [89.0, 90.0, 89.0]
-    grades.append(new_grades)
-
+    grades.extend(new_grades)
 
 
     # return student_info
@@ -71,21 +69,53 @@ def view_student_grades(students, grades):
     #         print(f"Subject: Science, Grade: ", student_info[1][2])
 
             
-def calculate_subject_averages():
+# def calculate_subject_averages():
+#     all_math_grades = []
+#     all_english_grades = []
+#     all_science_grades = []
+#     count = 0
+
+#     for grade in grades:
+#         math_individual_grade = grade[1][0]
+#         english_individual_grade = grade[1][1]
+#         science_individual_grade = grade[1][2]
+#         count += 1
+#         all_math_grades.append(math_individual_grade)
+#         all_english_grades.append(english_individual_grade)
+#         all_science_grades.append(science_individual_grade)
+
+#     if count > 0:
+#         average_math = sum(all_math_grades) / count
+#         print("Average Math Grade: ", average_math)
+#         average_english = sum(all_english_grades) / count
+#         print("Average English Grade: ", average_english)
+#         average_science = sum(all_science_grades) / count
+#         print("Average Science Grade: ", average_science)
+#     else:
+#         print("No students to calculate averages.")   
+
+
+def view_subject_averages(students, grades, new_grades):
     all_math_grades = []
     all_english_grades = []
     all_science_grades = []
+
     count = 0
 
-    for student_info in students:
-        math_individual_grade = student_info[1][0]
-        english_individual_grade = student_info[1][1]
-        science_individual_grade = student_info[1][2]
+    if not students:
+        print("No students to calculate averages.")
+        return
+
+    for new_grade in new_grades:
+        math_individual_grade = new_grade[1] if new_grade[0] == "Math" else 0
+        english_individual_grade = new_grade[1] if new_grade[0] == "English" else 0
+        science_individual_grade = new_grade[1] if new_grade[0] == "Science" else 0
         count += 1
         all_math_grades.append(math_individual_grade)
         all_english_grades.append(english_individual_grade)
         all_science_grades.append(science_individual_grade)
 
+    count = len(students)
     if count > 0:
         average_math = sum(all_math_grades) / count
         print("Average Math Grade: ", average_math)
@@ -94,14 +124,8 @@ def calculate_subject_averages():
         average_science = sum(all_science_grades) / count
         print("Average Science Grade: ", average_science)
     else:
-        print("No students to calculate averages.")   
-
-
-def view_subject_averages():
-    if not students:
         print("No students to calculate averages.")
-        return
-    calculate_subject_averages()
+
 
 def identify_top_performers():
     number_of_top_performer = input("Enter the number of top performer to show: ")
